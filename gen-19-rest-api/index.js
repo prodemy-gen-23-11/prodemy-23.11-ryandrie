be135d3633ef016166bafcd8f9f78f8e6c9c7519
Ryandrie Satria <mryandriesatriad@gmail.com>
2024-02-12 16:04:13 +0700

    upload tugas rest-api

--- gen-19-rest-api/index.js
+++ gen-19-rest-api/index.js
@@ -1,11 +1,20 @@
 const axios = require("axios");
 
-// Make a request for a user with a given ID
-axios
-    .get("https://dummyjson.com/products?limit=10")
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+// axios
+//     .get("https://dummyjson.com/products?limit=10")
+//     .then(function (response) {
+//         console.log(response.data);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+
+axios({
+    method: "get",
+    url: "https://dummyjson.com/products",
+    params: {
+        limit: 10,
+    },
+}).then((response) => {
+    console.log(response.data);
+});
